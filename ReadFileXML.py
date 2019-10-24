8d5c2793451ec1d98f263cc5ff25c58ee46e2aef
pmbibe <53257014+pmbibe@users.noreply.github.com>
2019-10-24 17:09:24 +0700

    Update ReadFileXML.py

--- ReadFileXML.py
+++ ReadFileXML.py
@@ -9,3 +9,5 @@ print "Errors: " + Result["errors"]
 print "Success: " + Result["assertions"]
 if Number_of_Errors > 0:
 	print "Can't deploy"
+else
+ 	os.system("rsync -v ./PHPUnit/*   -e ssh /  root@192.168.141.203:/usr/share/nginx/html")
